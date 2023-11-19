d75a389ecb2b92640371a2e3153c47530e602b56
Wendy-wyt <wuyuting0101@gmail.com>
2023-11-19 14:02:18 -0500

    modify mart__dim_bike_stations

--- nyc_transit/models/mart/mart__dim_bike_stations.sql
+++ nyc_transit/models/mart/mart__dim_bike_stations.sql
@@ -1,12 +1,16 @@
 -- ref: https://www.geeksforgeeks.org/how-to-select-the-first-row-of-each-group-by-in-sql/
 -- used in getting the first row for each station id
+-- Since we use SCD-I, we will consider the current value is the same as the value in the past.
+-- For the rows of the same station id, we will select the row 
+-- with the latest timestamp as the values for the station id.
 with bikeData as (
 select
     distinct
     start_station_id as station_id,
     start_station_name as station_name,
     start_lat station_lat,
-    start_lng station_lng
+    start_lng station_lng,
+    started_at_ts as ts
 from {{ ref('stg__bike_data') }}
 union
 select
@@ -14,7 +18,8 @@ select
     end_station_id as station_id,
     end_station_name as station_name,
     end_lat station_lat,
-    end_lng station_lng
+    end_lng station_lng,
+    ended_at_ts as ts
 from {{ ref('stg__bike_data') }}
 )
 select
@@ -25,7 +30,7 @@ select
 from (
     select 
         *,
-        ROW_NUMBER() OVER (PARTITION BY station_id ORDER BY station_name, station_lat, station_lng) AS rn
+        ROW_NUMBER() OVER (PARTITION BY station_id ORDER BY ts DESC, station_name, station_lat, station_lng) AS rn
     from bikeData
 )
 where rn=1 
